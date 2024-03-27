2789aa1982ee0a272e2dca142a5b7c70637abb30
elias-dzobo <elias.dzobo@gmail.com>
2024-03-27 09:02:06 +0000

    twitter bot updates - get data from s3 for now

--- twitter_bot.py
+++ twitter_bot.py
@@ -1,34 +1,63 @@
-import requests 
+
 import pandas as pd 
 import os 
 from dotenv import load_dotenv
+import tweepy
+from datetime import datetime
+import boto3
 
 load_dotenv()
 
 #key for authenticating twitter bot 
 API_KEY = os.getenv('API_KEY')
 API_SECRET_KEY = os.getenv('API_SECRET_KEY') 
+# Twitter API credentials
+CONSUMER_KEY = os.getenv('CONSUMER_KEY')
+CONSUMER_SECRET = os.getenv('CONSUMER_SECRET')
+ACCESS_TOKEN = os.getenv('ACCESS_TOKEN')
+ACCESS_TOKEN_SECRET = os.getenv('ACCESS_TOKEN_SECRET')
+#s3 credentials 
+AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
 
 
-data = {
-    'company': ['A', 'B', 'C', 'D'],
-    'price': [1,2,3,4],
-    'rev': [100, 200, 300, 400]
-}
+date = datetime.now().strftime("%d-%m-%Y")
+date = date.split('-')
+date[1] = date[1][1]
 
-df = pd.DataFrame(data)
+date = '-'.join(date)
 
-
-tweet_template = """
-Company  Price  Rev \n 
-"""
-
-for i in range(len(df)):
-    a = list(df.loc[i])
-    a = [str(i) for i in a]
-    x = "     ".join(a)
-    x += '\n'
-    tweet_template += x 
+# get s3 client
+s3 = boto3.client('s3',
+                  aws_access_key_id = AWS_ACCESS_KEY_ID,
+                  aws_secret_access_key=AWS_SECRET_ACCESS_KEY)
 
 
-print(tweet_template)
+filename = 'fuelprices_{}'.format(date)
+
+s3.download_file("elias-fuel-bucket", 'rawdata/{}'.format(filename), './{}'.format(filename))
+
+df = pd.read_csv(filename)
+
+#convert csv file in string template 
+tweet_message = ""
+
+
+
+
+
+# Authenticate to Twitter
+auth = tweepy.OAuth1UserHandler(CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+api = tweepy.API(auth)
+
+def tweet(message):
+    try:
+        api.update_status(message)
+        print("Tweeted:", message)
+    except tweepy.TweepError as e:
+        print("Error:", e)
+
+
+
+if __name__ == "__main__":
+    tweet(tweet_message)
