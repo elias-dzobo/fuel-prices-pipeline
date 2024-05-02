014396d6587160339ce7b440936b351bcddf0c4b
elias-dzobo <elias.dzobo@gmail.com>
2024-05-02 15:03:29 +0000

    functional data pipeline v1

--- data-scrape.py
+++ data-scrape.py
@@ -1,22 +1,20 @@
 import requests as re
-from pprint import pprint
 import json 
-from bs4 import BeautifulSoup
 from datetime import datetime 
 import pandas as pd 
-import boto3
 import os
-from dotenv import load_dotenv
+from dotenv import dotenv_values
+import psycopg2
 
-load_dotenv()
 
-AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
-AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
-AWS_REGION = os.getenv('AWS_REGION')
-S3_BUCKET_NAME = os.getenv('S3_BUCKET_NAME')
+env = dotenv_values('.env')
+
+
+
+
 #login details
-EMAIL = os.getenv('EMAIL')
-PASSWORD = os.getenv('PASSWORD')
+EMAIL = env.get('EMAIL')
+PASSWORD = env.get('PASSWORD')
 
 path = '/Users/eliasdzobo/Documents/data-eng/fuel-pipeline/Data'
 
@@ -24,9 +22,34 @@ path = '/Users/eliasdzobo/Documents/data-eng/fuel-pipeline/Data'
 This script logs into cedirates.com and scarpes data regarding fuel prices from several fuel stations in Ghana 
 """
 
-date = datetime.now().strftime("%d-%m-%Y")
+def format_date():
 
-s3 = boto3.client('s3')
+    date = datetime.now().strftime("%d-%m-%Y")
+    date = datetime.now().strftime("%d-%m-%Y")
+
+    date = date.split('-')
+    
+    if date[0][0] == '0':
+        date[0] = date[0][1]
+
+    if date[1][0] == '0':
+        date[1] = date[1][1]
+    
+
+    date = '-'.join(date)
+
+    return date 
+
+def db_connection():
+    conn = psycopg2.connect(
+    dbname=env.get("DB_NAME"),
+    user=env.get("DB_USER"),
+    password=env.get("DB_PASSWORD"),
+    host=env.get("DB_HOST"),
+    port=env.get("DB_PORT")
+    )
+
+    return conn 
 
 
 
@@ -39,18 +62,13 @@ def return_urls():
     Output: 
         String 
     """
-    date = datetime.now().strftime("%d-%m-%Y")
+    
 
-    date = date.split('-')
-    date[1] = date[1][1]
-    date[0] = date[0][1]
-
-    date = '-'.join(date)
+    date = format_date()
 
     url = "https://cedirates.com/api/v1/auth/login"
-    #scrape_url = "https://cedirates.com/api/v1/fuelPrices/{}".format(date) 
+    scrape_url = "https://cedirates.com/api/v1/fuelPrices/{}".format(date) 
 
-    scrape_url = "https://cedirates.com/api/v1/fuelPrices/8-4-2024"
 
     return url, scrape_url
 
@@ -111,42 +129,84 @@ def create_datafile(data_list):
         fuel_data["diesel"].append(entry['diesel'])
         fuel_data['petrol'].append(entry['petrol'])
 
+    date = format_date()
+
     filename = f'fuelprices_{date}.csv'
 
     pd.DataFrame(fuel_data).to_csv(os.path.join(path, filename))
 
     return filename
 
+def transform_data(filename):
+    df = pd.read_csv(filename)
 
-def save2S3bucket(filename):
+    date = format_date()
+
+    avg_petrol = round(df['petrol'].mean(), 2)
+    avg_diesel = round(df['diesel'].mean(), 2)
+
+    min_petrol = min(df['petrol']) 
+    min_diesel = min(df['diesel'])
+
+    max_petrol = max(df['petrol'])
+    max_diesel = max(df['diesel'])
+
+    min_petrol_station = df[df['petrol'] == min_petrol].values[0][1]
+    min_diesel_station = df[df['diesel'] == min_diesel].values[0][1]
+
+    max_petrol_station = df[df['petrol'] == max_petrol].values[0][1]
+    max_diesel_station = df[df['diesel'] == max_diesel].values[0][1]
+
+    ## save transformed metrics
+
+    return (date, avg_petrol, avg_diesel, min_petrol, min_diesel, max_petrol, max_diesel, min_petrol_station, min_diesel_station, max_petrol_station, max_diesel_station)
+
+def save_to_postgres(filename):
+    conn = db_connection()
+    curr = conn.cursor()
+
+    insert_query = """
+    INSERT INTO fuel_data 
+    VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
     """
-    A function that saves the data in csv format into an s3 bucket 
 
-    Input:
-        filename: String 
+    values_to_insert = transform_data(filename) 
 
-    Returns 
-        Bool
-    """
+    try:
 
-    try: 
-        #do exception handling 
-        s3.upload_file(filename, "elias-fuel-bucket", 'rawdata/{}'.format(filename))
+        curr.execute(insert_query, values_to_insert)
 
-        return True
+        conn.commit()
 
+        curr.close()
+        conn.close()
+
+        return True 
     except Exception as e:
-        return False
+        print('Exception: ', e)
+        return False 
+
+
+
 
 
 if __name__ == '__main__':
     url, scrape_url = return_urls()
 
+    print(url)
+    print(scrape_url)
     data_list = get_data(url, scrape_url)
 
-    filename = create_datafile(data_list)
+    file = create_datafile(data_list) 
 
-    #save2S3bucket(filename)
+
+    path = os.path.join(path, file)
+    save_to_postgres(path)
+
+    #filename = create_datafile(data_list)
+
+
+
 
 
 
--- transform.py
+++ transform.py
@@ -18,5 +18,31 @@ df = pd.read_csv(os.path.join(path, x[0]))
 avg_petrol = round(df['petrol'].mean(), 2)
 avg_diesel = round(df['diesel'].mean(), 2)
 
-print(avg_diesel)
-print(avg_petrol)
+min_petrol = min(df['petrol']) 
+min_diesel = min(df['diesel'])
+
+max_petrol = max(df['petrol'])
+max_diesel = max(df['diesel'])
+
+min_petrol_station = df[df['petrol'] == min_petrol].values[0][1]
+min_diesel_station = df[df['diesel'] == min_diesel].values[0][1]
+
+max_petrol_station = df[df['petrol'] == max_petrol].values[0][1]
+max_diesel_station = df[df['diesel'] == max_diesel].values[0][1]
+
+
+print('avg diesel ', avg_diesel)
+print('avg petrol ',avg_petrol)
+
+print('min petrol ',min_petrol)
+print('min diesel ',min_diesel)
+
+print('max petrol ',max_petrol)
+print('max diesel ',max_diesel)
+
+print('min petrol station ',min_petrol_station)
+print('min diesel station ',min_diesel_station)
+
+print('max petrol station ',max_petrol_station)
+print('max diesel station ',max_diesel_station)
+
